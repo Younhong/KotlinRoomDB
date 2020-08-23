498feb9c809dc26fab78a8d54dc878c06bc50237
Younhong <younhong@kakao.com>
2020-08-23 18:13:17 +0900

    main activity 추가

--- app/src/main/java/com/example/memo/MainActivity.kt
+++ app/src/main/java/com/example/memo/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.memo
 
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
+@SuppressLint("StaticFieldLeak")
 class MainActivity : AppCompatActivity() {
+
+    lateinit var db: MemoDatabase
+    var memoList = listOf<MemoEntity>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db = MemoDatabase.getInstance(this)!!
+
+        button_add.setOnClickListener {
+            val memo = MemoEntity(null, edit_text_memo.text.toString())
+            insertMemo(memo)
+        }
+    }
+
+    fun insertMemo(memo : MemoEntity) {
+        val insertTask = object : AsyncTask<Unit, Unit, Unit>() {
+            override fun doInBackground(vararg params: Unit?) {
+                db.memoDAO().insert(memo)
+            }
+
+            override fun onPostExecute(result: Unit?) {
+                super.onPostExecute(result)
+                getAllMemo()
+            }
+        }
+
+        insertTask.execute()
+    }
+
+    fun getAllMemo() {
+        val getTask = (object : AsyncTask<Unit, Unit, Unit>(){
+            override fun doInBackground(vararg params: Unit?) {
+                memoList = db.memoDAO().getAll()
+            }
+
+            override fun onPostExecute(result: Unit?) {
+                super.onPostExecute(result)
+                setRecyclerView(memoList)
+            }
+        }).execute()
+    }
+
+    fun deleteMemo() {
+
+    }
+
+    fun setRecyclerView(memoList: List<MemoEntity>) {
+
     }
 }
